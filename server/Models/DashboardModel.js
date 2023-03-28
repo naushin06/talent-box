7cf5762108e4bce78c4cf62a27d211805737948f
naushin <nnaushin2002@gmail.com>
2023-03-28 14:45:37 +0530

    deleting submodules

--- server/Models/DashboardModel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const dashboardSchema = new mongoose.Schema({
-  first_name: String
-});
-
-module.exports = mongoose.model("Dashboard", dashboardSchema);
-  
