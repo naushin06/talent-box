48059d52bae02686556bceb0fda940a41e0f88a4
naushin <nnaushin2002@gmail.com>
2023-03-29 15:41:16 +0530

    adding the dependencies

--- talentbox-server/app.js
+++ talentbox-server/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
 app.get("/", (req, res) => {
   res.send("Express on Vercel");
 });
