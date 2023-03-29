8826ac311f653c6a3df5611618f99c7c696bd136
naushin <nnaushin2002@gmail.com>
2023-03-29 14:31:30 +0530

    for getting displayed on the front

--- talentbox-server/app.js
+++ talentbox-server/app.js
@@ -19,6 +19,9 @@ const cookieParser = require("cookie-parser");
 app.listen(4000, () => {
   console.log("Server listening on port 4000");
 });
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
+});
 app.use(
   cors({
     origin: ["http://localhost:3000"],
