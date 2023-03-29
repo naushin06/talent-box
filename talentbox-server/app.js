571b55553d5fa550de087ccf8259eef6eeba5da4
naushin <nnaushin2002@gmail.com>
2023-03-29 14:18:51 +0530

    vercel deplyment

--- talentbox-server/app.js
+++ talentbox-server/app.js
@@ -1,42 +1,54 @@
 const express = require("express");
-const cors = require("cors");
 const app = express();
-const genericRoutes = require("./routes/commonRoutes");
-const mongoose = require("mongoose");
-const cookieParser = require("cookie-parser");
-app.listen(4000, () => {
-  console.log("Server listening on port 4000");
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
 });
-app.use(
-  cors({
-    origin: ["http://localhost:3000"],
-    methods: ["GET", "POST"],
-    credentials: true,
-  })
-);
-try {
-  mongoose
-    .connect(
-      "mongodb+srv://root:password123*@cluster0.hxeftjt.mongodb.net/talentbox",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    )
-    .then(() => {
-      console.log("Db CONNECT");
-    });
-} catch (error) {
-  console.log(error.message);
-}
-// Close the Mongoose connection when the API server is shut down
-process.on("SIGTERM", () => {
-  app.close(() => {
-    console.log("API server shut down");
-    mongoose.disconnect();
-  });
+app.listen(5000, () => {
+  console.log("Running on port 5000.");
 });
+// Export the Express API
+module.exports = app;
 
-app.use(cookieParser());
-app.use(express.json());
-app.use("/talentbox", genericRoutes);
+
+// const express = require("express");
+// const cors = require("cors");
+// const app = express();
+// const genericRoutes = require("./routes/commonRoutes");
+// const mongoose = require("mongoose");
+// const cookieParser = require("cookie-parser");
+// app.listen(4000, () => {
+//   console.log("Server listening on port 4000");
+// });
+// app.use(
+//   cors({
+//     origin: ["http://localhost:3000"],
+//     methods: ["GET", "POST"],
+//     credentials: true,
+//   })
+// );
+// try {
+//   mongoose
+//     .connect(
+//       "mongodb+srv://root:password123*@cluster0.hxeftjt.mongodb.net/talentbox",
+//       {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//       }
+//     )
+//     .then(() => {
+//       console.log("Db CONNECT");
+//     });
+// } catch (error) {
+//   console.log(error.message);
+// }
+// // Close the Mongoose connection when the API server is shut down
+// process.on("SIGTERM", () => {
+//   app.close(() => {
+//     console.log("API server shut down");
+//     mongoose.disconnect();
+//   });
+// });
+
+// app.use(cookieParser());
+// app.use(express.json());
+// app.use("/talentbox", genericRoutes);
